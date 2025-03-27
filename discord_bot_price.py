5095a05c4cfe6c5c2184ebd56e3d6b27b518bfc8
Kostas2104 <138314749+Kostas2104@users.noreply.github.com>
2025-03-27 02:14:03 +0200

    Update discord_bot_price.py

--- discord_bot_price.py
+++ discord_bot_price.py
@@ -90,7 +90,7 @@ async def cdc(ctx):
         conn = get_db_connection()
         cur = conn.cursor()
         now = datetime.now()
-       cur.execute("""
+        cur.execute("""
             INSERT INTO caw_cdc (wallet_3da3, wallet_667f, wallet_825b, sum, date, time)
             VALUES (%s, %s, %s, %s, %s, %s)
         """, (cdc_balances[0], cdc_balances[1], cdc_balances[2], cdc_total, now.date(), now.time()))
