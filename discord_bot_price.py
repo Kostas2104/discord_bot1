8c94ceced652e50c9ecb0546fefc5024bed47c31
Kostas2104 <138314749+Kostas2104@users.noreply.github.com>
2025-06-29 23:27:55 +0300

    Update discord_bot_price.py

--- discord_bot_price.py
+++ discord_bot_price.py
@@ -73,7 +73,7 @@ Use `!helpme` anytime to see this list again.
 # 📌 CDC Wallet Titles (excluding Burn)
 CDC_WALLET_TITLES = ["wallet_3da3", "wallet_677f", "wallet_825b", "wallet_7fc6"]
 MC_TITLE = "Market Cap"
-GATE_IO_TITLE = "wallet_92Fe"
+GATE_IO_TITLE = ["wallet_92Fe"]
 
 # 📌 Format number to Trillions (T)
 def format_trillions(value):
